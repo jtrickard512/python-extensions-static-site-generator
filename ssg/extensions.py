79f91ae717f5d05090d0cf2b9d9bcd7a9a42a720
jtrickard <45494119+jtrickard@users.noreply.github.com>
2021-06-24 09:26:52 -0400

    updated

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -3,7 +3,7 @@ import importlib
 from pathlib import Path
 
 def load_module(directory,name):
-    sys.path.insert(directory)[0]
+    sys.path.insert(0,directory)
     importlib.import_module(name)
     sys.path.pop(0)
 
