9ed03aa8d55b43ebded40a610240319378c94c22
jtrickard <45494119+jtrickard@users.noreply.github.com>
2021-06-24 11:04:55 -0400

    updated

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -4,7 +4,7 @@ import time
 start_time = None
 total_written = 0
 
-@hooks.register("start_buld")
+@hooks.register("start_build")
 def start_build():
     global start_time
     start_time = time.time()
